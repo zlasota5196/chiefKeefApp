a9f75ba88121c7deaf4f74edf4befc226d19118b
Zuzanna Lasota <zlasota5196@stu.d214.org>
2022-04-28 14:44:31 -0500

    array made

--- chiefKeefApp/GameScene.swift
+++ chiefKeefApp/GameScene.swift
@@ -14,6 +14,7 @@ class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
         player = self.childNode(withName: "greenGloGuy") as! SKSpriteNode
+        var blockArray = ["block1","block2","block3","block4","block5","block6","block7","block8"]
         
         backgroundColor = SKColor.purple
         
