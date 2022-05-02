d17f544f8f91ad33dee213d4547ab4dbeb98dd4d
Caity Lin Bouris <cbouris3001@stu.d214.org>
2022-05-02 14:34:24 -0500

    little jump

--- chiefKeefApp/GameScene.swift
+++ chiefKeefApp/GameScene.swift
@@ -13,10 +13,16 @@ class GameScene: SKScene {
     var player = SKSpriteNode()
     
     override func didMove(to view: SKView) {
-        player = self.childNode(withName: "greenGloGuy") as! SKSpriteNode
+        player = self.childNode(withName: "gloGuy") as! SKSpriteNode
         
         backgroundColor = SKColor.purple
         
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            player.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 60.0))
+        }
     }
     
     
